db457423469143a9987035a5b2a02d4a0a08905d
VicSwain <152931791+VicSwain@users.noreply.github.com>
2024-03-10 21:57:54 -0500

    added code to read, write, and append notes to fsUtils; roues to index; get, post to notes

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+
 const notesRouter = require('./notes');
 
 router.use('/notes', notesRouter);
--- routes/notes.js
+++ routes/notes.js
@@ -1,7 +1,28 @@
 const notes = require('express').Router();
 const { readFromFile, readAndAppend } = require('../helpers/fsUtils');
+
 // helper function to generate unique ids
 const uuid = require('../helpers/uuid');
 
+notes.get('/', (req, res) => {
+    readFromFile('./db.json').then((data) => res.json(JSON.parse(data)));
+});
+
+notes.post('/', (req, res) => {
+    const { title, text } = req.body;
+    
+    if (req.body) {
+        const newNote = {
+            title,
+            text,
+            note_id: uuid(),
+        };
+
+        readAndAppend(newNote, './db.json');
+        res.json('Note added successfully!');
+    } else {
+        res.errored('Error in adding note');
+    }
+});
 
 module.exports = notes;
