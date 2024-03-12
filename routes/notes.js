0bf1b53ca9e92b0c3785010d13cc0e627b418a27
VicSwain <152931791+VicSwain@users.noreply.github.com>
2024-03-12 16:58:06 -0500

    attempt to bring up previous notes not working

--- routes/notes.js
+++ routes/notes.js
@@ -1,15 +1,19 @@
 const notes = require('express').Router();
 const { readFromFile, readAndAppend } = require('../helpers/fsUtils');
-
 // helper function to generate unique ids
 const uuid = require('../helpers/uuid');
-
-
-
 
 notes.get('/', (req, res) => {
     readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
 });
+
+notes.get('/:id', (req,res) => {
+    const requestedNote = req.params.id;
+    console.log(requestedNote);
+}
+);
+
+
 
 notes.post('/', (req, res) => {
     const { title, text } = req.body;
@@ -29,13 +33,7 @@ notes.post('/', (req, res) => {
 });
 
 
-notes.get('/api/:note_id', (req, res) => {
-    const requestedNote = req.params.note_id;
-    console.log(requestedNote);
-    readFromFile('./db/db.json/:note_id').then((data) => res.json(JSON.parse(data)));
-    
-});
-//note_id is what I feel should be used to pick the specific delete
+
 
 
 module.exports = notes;
